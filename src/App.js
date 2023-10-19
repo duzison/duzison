c47c12ad25e5921c09ae1c5251320daaadb59809
duzison <sojin.son96@gmail.com>
2023-10-19 16:59:16 +0900

    커밋 연습

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState, useRef, useCallback } from 'react';
 const App = () => {
   const [todos, setTodos] = useState([
     { id: 1, text: '리액트 기초 알아보기', checked: true },
-    { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
+    { id: 2, text: '컴포넌트 스타일링해 보기', checked: false },
     { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
   ]);
 
